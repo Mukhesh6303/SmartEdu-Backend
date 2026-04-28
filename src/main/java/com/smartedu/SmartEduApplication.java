e5975b256627de8d0395d2762e63c0f18f0e9c67
K Mohan Mukhesh <2400032721@kluniversity.in>
2026-04-28 10:27:15 +0530

    Remove duplicate CORS bean from SmartEduApplication

--- src/main/java/com/smartedu/SmartEduApplication.java
+++ src/main/java/com/smartedu/SmartEduApplication.java
@@ -2,9 +2,6 @@ package com.smartedu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SmartEduApplication {
@@ -13,17 +10,4 @@ public class SmartEduApplication {
 		SpringApplication.run(SmartEduApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOriginPatterns("*")
-						.allowedMethods("*")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
-	}
 }
